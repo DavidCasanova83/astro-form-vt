a4b8790d0582830715e0810c661035bffa2abac1
David Casanova <casanova.83130@gmail.com>
2025-02-12 19:18:50 +0100

    add info location dans la réponse

--- api/saveAnswer.js
+++ api/saveAnswer.js
@@ -23,7 +23,6 @@ export default async function handler(req, res) {
     const item = {
       primaire: uuidv4(), // Clé primaire unique
       ...data,
-      formulaire: 'toulon',  // Ajoute le champ "formulaire"
       timestamp: new Date().toISOString(),
     };
     console.log("Item à insérer :", item);
